e3318d96e8d8fa1653748d5f4c0c234774f0ac15
DevJsmJsr <jsmajsr@outlook.com>
2024-11-13 21:21:27 -0500

    add shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),
       '~assets': path.resolve(__dirname, './src/assets'),
       '~components': path.resolve(__dirname, './src/components'),
       '~constants': path.resolve(__dirname, './src/constants'),
@@ -17,8 +18,7 @@ export default defineConfig({
       '~pages': path.resolve(__dirname, './src/pages'),
       '~routes': path.resolve(__dirname, './src/routes'),
       '~types': path.resolve(__dirname, './src/types'),
-      '~utils': path.resolve(__dirname, './src/utils'),
-      '~store': path.resolve(__dirname, './src/store')
+      '~utils': path.resolve(__dirname, './src/utils')
     }
   },
   server: { port: 3000, host: true },
