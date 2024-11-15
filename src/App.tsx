f3738ed0394603780657f25a10a3700add7529f0
DevJsmJsr <jsmajsr@outlook.com>
2024-11-14 20:40:02 -0500

    render lading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Routes from '~routes/Routes';
-
+import reactLogo from "~assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Routes from "~routes/Routes";
 
 function App() {
-
   return (
     <>
-    <Routes/>
+      <Routes />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
